438973bdd63e6c19b2b5c4da23daf2e6cfa5710c
Rodronpc <rodrigogarcia150105@gmail.com>
2024-10-11 13:21:11 -0600

    Update ejercicio9.2.c

--- ejercicio9.2.c
+++ ejercicio9.2.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main ()
 {
-int lista[5] = {10, 8, 5, 8, 7}; 
-int indice = 0;
-printf("\tLista\n");
-do 
-{
-printf("\nCalificación del alumno %d es %d", indice+1, lista[indice]);
-indice += 1; 
-while (indice < 5 );
-printf("\n");
-return 0;
+  int lista[5] = {10, 8, 5, 8, 7}; 
+  int indice = 0;
+  printf("\tLista\n");
+  do 
+  {
+    printf("\nCalificación del alumno %d es %d", indice+1, lista[indice]);
+    indice += 1; 
+  }
+  while (indice < 5 );
+  printf("\n");
+  return 0;
 }
